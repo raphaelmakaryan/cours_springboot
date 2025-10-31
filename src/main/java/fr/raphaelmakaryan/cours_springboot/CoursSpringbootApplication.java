a5830997349c3d56c4e5de81fefbbc176bbfd476
raphaelmakaryan <contact@raphaelmakaryan.fr>
2025-10-31 15:55:45 +0100

    Résolution de cet erreur de merde

--- src/main/java/fr/raphaelmakaryan/cours_springboot/CoursSpringbootApplication.java
+++ src/main/java/fr/raphaelmakaryan/cours_springboot/CoursSpringbootApplication.java
@@ -1,10 +1,21 @@
 package fr.raphaelmakaryan.cours_springboot;
 
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
 
 @SpringBootApplication
 public class CoursSpringbootApplication {
+
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
+
+    @Value("${spring.application.name}")
+    private String appName;
 
     public static void main(String[] args) {
         SpringApplication.run(CoursSpringbootApplication.class, args);
