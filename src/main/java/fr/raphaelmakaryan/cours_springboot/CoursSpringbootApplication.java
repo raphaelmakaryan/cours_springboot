934aeba04a6433ba3688f70b0c8b9bfcb4e9653f
raphaelmakaryan <contact@raphaelmakaryan.fr>
2025-10-31 08:10:08 +0100

    Init project

---
+++ src/main/java/fr/raphaelmakaryan/cours_springboot/CoursSpringbootApplication.java
@@ -0,0 +1,13 @@
+package fr.raphaelmakaryan.cours_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/raphaelmakaryan/cours_springboot/CoursSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.raphaelmakaryan.cours_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
